cdbf44373d52e10ff3e3be9c7543c81010248194
Ярослав Гаркуша <ygarkusha@web100-ck.local>
2019-04-18 13:16:54 +0300

    restored motion improve

--- app/fruits/fruit.js
+++ app/fruits/fruit.js
@@ -7,18 +7,50 @@ class Fruit{
         this.id = '';
     }
 
+    setShape(shape){
+        this.shape = shape;
+    }
+
+    initMovement(){
+        const randomAdditionSpeed = Math.floor(Math.random() * (options.maxSpeed - options.minSpeed));
+        this.speed = 450 + randomAdditionSpeed;
+        const middleX = options.w / 2;
+        const b = options.h / 2;
+        const a = (this.shape.x < middleX) ? (middleX - this.shape.x) : (this.shape.x - middleX);
+        const tan = b / a;
+        const angle = Math.atan(tan);
+        this.speedY = Math.sin(angle) * this.speed;
+        this.speedX = (1 - Math.sin(angle)) * this.speed;
+        if(this.shape.x > middleX){
+            this.speedX *= -1;
+        }
+        this.rotationSpeed = Math.floor(Math.random() * options.maxRotationSpeed);
+    }
+
+    move(){
+        this.shape.y -= this.speedY / createjs.Ticker.framerate;
+        this.shape.x += this.speedX / createjs.Ticker.framerate;
+        this.speedY += options.acceleration / createjs.Ticker.framerate;
+        this.shape.rotation += this.rotationSpeed / createjs.Ticker.framerate;
+    }
+
+    isOut(){
+        if(Math.abs(this.speedY) > this.speed && this.shape.y > options.h){
+            return true;
+        }
+        return false;
+    }
+
+    removeShape(){
+        this.shape.dispatchEvent('fruitout');
+    }
+
     generatePath(imgWidth){
         const xLeftRange = [imgWidth / 2, options.w/3];
-        const yTopRange = [100, options.h / 2];
         const xRightRange = [2 * options.w/3, options.w - imgWidth / 2];
 
         const leftBottomX = Math.round(Math.random() * (xLeftRange[1] - xLeftRange[0])) + xLeftRange[0];
-        const leftTopX = Math.round(Math.random() * (xLeftRange[1] - xLeftRange[0])) + xLeftRange[0];
         const rightBottomX = Math.round(Math.random() * (xRightRange[1] - xRightRange[0])) + xRightRange[0];
-        const rightTopX = Math.round(Math.random() * (xRightRange[1] - xRightRange[0])) + xRightRange[0];
-        const bottomY = options.h + 50;
-        const topY = Math.round(Math.random() * (yTopRange[1] - yTopRange[0])) + yTopRange[0];
-        const middleTopX = Math.round((leftTopX + rightTopX)/2);
 
         const path = {};
 
@@ -27,26 +59,12 @@ class Fruit{
         */
         if(Math.random() > 0.5){
             path.up = [
-                leftBottomX, bottomY,
-                leftTopX, topY,
-                middleTopX, topY
-            ];
-            path.down = [
-                middleTopX, topY,
-                rightTopX, topY,
-                rightBottomX, bottomY
+                leftBottomX
             ];
         }else{
             path.up = [
-                rightBottomX, bottomY,
-                rightTopX, topY,
-                middleTopX, topY
+                rightBottomX
             ];
-            path.down = [
-                middleTopX, topY,
-                leftTopX, topY,
-                leftBottomX, bottomY
-            ]
         }
         
         return path;
